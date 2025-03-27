18cc0cc4eb732204f3531e6475db54e57f856cf9
Chirag <154595968+Chirag-R-Karanth@users.noreply.github.com>
2025-03-27 15:30:05 +0000

    closed rectangle problem

---
+++ Shapes.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Shapes
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int len=sc.nextInt();
+        int bre=sc.nextInt();
+        rectangle.make(len,bre);
+    }
+}
+
+class rectangle
+{
+    static void make(int len, int bre)
+    {
+        for(int j=0 ; j<bre ;j++)
+        {
+            for(int i=0; i<len ; i++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
